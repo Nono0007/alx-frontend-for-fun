55eb68d5f5ae9e9511825de8bd29817c8fbc0c87
Nono0007 <noreenmagaya24@gmail.com>
2023-07-15 21:03:26 +0000

    markdown2html.py

---
+++ markdown2html.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+This script codes the markdown to HTML
+'''
+import sys
+import os
+import re
+
+if __name__ == '__main__':
+
+    # Test if the number of arguments passed is 2
+    if len(sys.argv[1:] != 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        sys.exit(1)
+
+    # Store the arguments into variables
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Check that the markdown file exist and it is a file
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
